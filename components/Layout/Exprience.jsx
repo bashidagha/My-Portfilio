07b97adbfba2c835752cc9a8817245a69142114e
Farshid Karimi <farshidk49@gmail.com>
2022-08-18 00:25:51 +0430

    exprience added

--- components/Layout/Exprience.jsx
+++ components/Layout/Exprience.jsx
@@ -11,6 +11,32 @@ const Exprience = () => {
       className={`tile ${classes.exprience}`}
     >
       <h4>Experiences</h4>
+
+      <div className={classes["exprience-item"]}>
+        <img src="pantherx.png" alt="pantherx"></img>
+
+        <div>
+          <p>Feb 2017 - Current</p>
+          <h5>Front-end developer</h5>
+          <p>
+            Donec aliquam est dui, vel vestibulum diam sollicitudin id. Quisque
+            feugiat malesuada molestie.
+          </p>
+        </div>
+      </div>
+
+      <div className={classes["exprience-item"]}>
+        <img src="bug.jpg" alt="bug"></img>
+
+        <div>
+          <p>Aug 2016 - Feb 2018</p>
+          <h5>Full-stack developer</h5>
+          <p>
+            Donec aliquam est dui, vel vestibulum diam sollicitudin id. Quisque
+            feugiat malesuada molestie.
+          </p>
+        </div>
+      </div>
     </AnimationOnScroll>
   );
 };
