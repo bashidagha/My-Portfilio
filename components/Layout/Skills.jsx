170ad3524e314c053972e5bb48d1255da85b1a0c
Farshid Karimi <farshidk49@gmail.com>
2022-08-18 16:18:51 +0430

    add other skills

--- components/Layout/Skills.jsx
+++ components/Layout/Skills.jsx
@@ -2,7 +2,122 @@ import React from "react";
 import { AnimationOnScroll } from "react-animation-on-scroll";
 import classes from "./skills.module.css";
 
-const Skills = () => {
+const Skills = (props) => {
+  const ComponentA = () => {
+    return (
+      <>
+        <h4>Front end</h4>
+
+        <div className={`${classes["skill-item"]}`}>
+          <p>React / Redux</p>
+          <div className={`${classes.progress}`}>
+            <div />
+          </div>
+        </div>
+
+        <div className={`${classes["skill-item"]}`}>
+          <p>React Router/ hooks</p>
+          <div className={`${classes.progress}`}>
+            <div />
+          </div>
+        </div>
+
+        <div className={`${classes["skill-item"]}`}>
+          <p>HTML / CSS / JS / TS</p>
+          <div className={`${classes.progress}`}>
+            <div />
+          </div>
+        </div>
+
+        <div className={`${classes["skill-item"]}`}>
+          <p>Responsive Design</p>
+          <div className={`${classes.progress}`}>
+            <div />
+          </div>
+        </div>
+
+        <div className={`${classes["skill-item"]}`}>
+          <p>Nextjs</p>
+          <div className={`${classes.progress}`}>
+            <div />
+          </div>
+        </div>
+
+        <div className={`${classes["skill-item"]}`}>
+          <p>Sass</p>
+          <div className={`${classes.progress}`}>
+            <div />
+          </div>
+        </div>
+
+        <div className={`${classes["skill-item"]}`}>
+          <p>Bootstrap</p>
+          <div className={`${classes.progress}`}>
+            <div />
+          </div>
+        </div>
+      </>
+    );
+  };
+
+
+  const ComponentB = () => {
+    return (
+      <>
+        <h4>Other Skills</h4>
+
+        <div className={`${classes["skill-item"]}`}>
+          <p>Java / Android</p>
+          <div className={`${classes.progress}`}>
+            <div />
+          </div>
+        </div>
+
+        <div className={`${classes["skill-item"]}`}>
+          <p>C / C++</p>
+          <div className={`${classes.progress}`}>
+            <div />
+          </div>
+        </div>
+
+        <div className={`${classes["skill-item"]}`}>
+          <p>Embedded Systems</p>
+          <div className={`${classes.progress}`}>
+            <div />
+          </div>
+        </div>
+
+        <div className={`${classes["skill-item"]}`}>
+          <p>Linux </p>
+          <div className={`${classes.progress}`}>
+            <div />
+          </div>
+        </div>
+
+        <div className={`${classes["skill-item"]}`}>
+          <p>REST‌ API/ Docker/ Git</p>
+          <div className={`${classes.progress}`}>
+            <div />
+          </div>
+        </div>
+
+        <div className={`${classes["skill-item"]}`}>
+          <p>Wordpress/Seo</p>
+          <div className={`${classes.progress}`}>
+            <div />
+          </div>
+        </div>
+
+        <div className={`${classes["skill-item"]}`}>
+          <p>Nodejs(Backend)</p>
+          <div className={`${classes.progress}`}>
+            <div />
+          </div>
+        </div>
+      </>
+    );
+  };
+
   return (
     <AnimationOnScroll
       animateIn="animate__tada"
@@ -10,56 +125,9 @@ const Skills = () => {
       initiallyVisible={true}
       className={`tile ${classes.skills}`}
     >
-      <h4>Front end</h4>
-
-      <div className={`${classes["skill-item"]}`}>
-        <p>React / Redux</p>
-        <div className={`${classes.progress}`}>
-          <div />
-        </div>
-      </div>
-
-      <div className={`${classes["skill-item"]}`}>
-        <p>React Router/ hooks</p>
-        <div className={`${classes.progress}`}>
-          <div />
-        </div>
-      </div>
-
-      <div className={`${classes["skill-item"]}`}>
-        <p>HTML / CSS / JS</p>
-        <div className={`${classes.progress}`}>
-          <div />
-        </div>
-      </div>
-
-      <div className={`${classes["skill-item"]}`}>
-        <p>Responsive Design</p>
-        <div className={`${classes.progress}`}>
-          <div />
-        </div>
-      </div>
-
-      <div className={`${classes["skill-item"]}`}>
-        <p>Nextjs</p>
-        <div className={`${classes.progress}`}>
-          <div />
-        </div>
-      </div>
-
-      <div className={`${classes["skill-item"]}`}>
-        <p>Sass</p>
-        <div className={`${classes.progress}`}>
-          <div />
-        </div>
-      </div>
-
-      <div className={`${classes["skill-item"]}`}>
-        <p>Bootstrap</p>
-        <div className={`${classes.progress}`}>
-          <div />
-        </div>
-      </div>
+      {props.type!=="2" && <ComponentA/>}
+      {props.type==="2" && <ComponentB/>}
+      
     </AnimationOnScroll>
   );
 };
