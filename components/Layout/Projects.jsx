f36ffc6cb9a7aca970704dd0ede9d64d0e1d3bb4
Farshid Karimi <farshidk49@gmail.com>
2022-08-18 12:24:37 +0430

    projects ui done

--- components/Layout/Projects.jsx
+++ components/Layout/Projects.jsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { AnimationOnScroll } from "react-animation-on-scroll";
 import classes from "./projects.module.css";
 
 const myprojects = [
   {
     hash: ["HTML", "CSS", "Responsive"],
-    title: "Recipe Blog",
+    title: "Recipe Blog1",
     description:
       "In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io",
     demo: "https://bashidagha.github.io/recipe-page-master/",
@@ -14,7 +14,7 @@ const myprojects = [
   },
   {
     hash: ["HTML", "CSS", "Responsive"],
-    title: "Recipe Blog",
+    title: "Recipe Blog2",
     description:
       "In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io",
     demo: "https://bashidagha.github.io/recipe-page-master/",
@@ -23,7 +23,7 @@ const myprojects = [
   },
   {
     hash: ["HTML", "CSS", "Responsive"],
-    title: "Recipe Blog",
+    title: "Recipe Blog3",
     description:
       "In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io",
     demo: "https://bashidagha.github.io/recipe-page-master/",
@@ -32,7 +32,7 @@ const myprojects = [
   },
   {
     hash: ["HTML", "CSS", "Responsive"],
-    title: "Recipe Blog",
+    title: "Recipe Blog4",
     description:
       "In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io",
     demo: "https://bashidagha.github.io/recipe-page-master/",
@@ -41,7 +41,7 @@ const myprojects = [
   },
   {
     hash: ["HTML", "CSS", "Responsive"],
-    title: "Recipe Blog",
+    title: "Recipe Blog5",
     description:
       "In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io",
     demo: "https://bashidagha.github.io/recipe-page-master/",
@@ -50,7 +50,7 @@ const myprojects = [
   },
   {
     hash: ["HTML", "CSS", "Responsive"],
-    title: "Recipe Blog",
+    title: "Recipe Blog6",
     description:
       "In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io",
     demo: "https://bashidagha.github.io/recipe-page-master/",
@@ -59,7 +59,7 @@ const myprojects = [
   },
   {
     hash: ["HTML", "CSS", "Responsive"],
-    title: "Recipe Blog",
+    title: "Recipe Blog7",
     description:
       "In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io",
     demo: "https://bashidagha.github.io/recipe-page-master/",
@@ -68,7 +68,7 @@ const myprojects = [
   },
   {
     hash: ["HTML", "CSS", "Responsive"],
-    title: "Recipe Blog",
+    title: "Recipe Blog8",
     description:
       "In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io",
     demo: "https://bashidagha.github.io/recipe-page-master/",
@@ -77,7 +77,7 @@ const myprojects = [
   },
   {
     hash: ["HTML", "CSS", "Responsive"],
-    title: "Recipe Blog",
+    title: "Recipe Blog9",
     description:
       "In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io",
     demo: "https://bashidagha.github.io/recipe-page-master/",
@@ -89,6 +89,8 @@ const myprojects = [
 const Projects = () => {
   const pagesArray = Array.from(Array(Math.ceil(myprojects.length / 3)).keys());
 
+  const [currentPage, setCurrentPage] = useState(1);
+
   return (
     <AnimationOnScroll
       animateIn="animate__fadeInRightBig"
@@ -97,17 +99,36 @@ const Projects = () => {
       className={classes.projects}
     >
       <div className={classes.projects__container}>
-        <div className="tile"></div>
+        {myprojects
+          .slice((currentPage - 1) * 3, currentPage * 3)
+          .map((project) => {
+            return (
+              <div className={`tile ${classes.project__item}`}>
+                <img src={project.photo} alt={project.title}></img>
+                {project.hash.map((hash) => (
+                  <span>#{hash}&nbsp;&nbsp;</span>
+                ))}
+                <h3>{project.title}</h3>
+                <p>{project.description}</p>
 
-        <div className="tile"></div>
+                <div className={classes.project__links}>
+                  <a>Demo</a>
+                  <a>Code</a>
+                </div>
 
-        <div className="tile"></div>
+              </div>
+            );
+          })}
       </div>
 
       <div className={classes.pagination}>
         <div className={classes.page}>{"<"}</div>
         {pagesArray.map((page) => (
-          <div className={classes.page} key={page}>
+          <div
+            className={classes.page}
+            key={page}
+            onClick={() => setCurrentPage(page + 1)}
+          >
             {page + 1}
           </div>
         ))}
