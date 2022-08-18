a68e9bf2f447d53bfb8ca23857a4b8dd7afca61a
Farshid Karimi <farshidk49@gmail.com>
2022-08-18 11:57:43 +0430

    pagintaion added

--- components/Layout/Projects.jsx
+++ components/Layout/Projects.jsx
@@ -2,7 +2,93 @@ import React from "react";
 import { AnimationOnScroll } from "react-animation-on-scroll";
 import classes from "./projects.module.css";
 
+const myprojects = [
+  {
+    hash: ["HTML", "CSS", "Responsive"],
+    title: "Recipe Blog",
+    description:
+      "In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io",
+    demo: "https://bashidagha.github.io/recipe-page-master/",
+    code: "https://github.com/bashidagha/recipe-page-master",
+    photo: "p1.webp",
+  },
+  {
+    hash: ["HTML", "CSS", "Responsive"],
+    title: "Recipe Blog",
+    description:
+      "In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io",
+    demo: "https://bashidagha.github.io/recipe-page-master/",
+    code: "https://github.com/bashidagha/recipe-page-master",
+    photo: "p1.webp",
+  },
+  {
+    hash: ["HTML", "CSS", "Responsive"],
+    title: "Recipe Blog",
+    description:
+      "In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io",
+    demo: "https://bashidagha.github.io/recipe-page-master/",
+    code: "https://github.com/bashidagha/recipe-page-master",
+    photo: "p1.webp",
+  },
+  {
+    hash: ["HTML", "CSS", "Responsive"],
+    title: "Recipe Blog",
+    description:
+      "In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io",
+    demo: "https://bashidagha.github.io/recipe-page-master/",
+    code: "https://github.com/bashidagha/recipe-page-master",
+    photo: "p1.webp",
+  },
+  {
+    hash: ["HTML", "CSS", "Responsive"],
+    title: "Recipe Blog",
+    description:
+      "In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io",
+    demo: "https://bashidagha.github.io/recipe-page-master/",
+    code: "https://github.com/bashidagha/recipe-page-master",
+    photo: "p1.webp",
+  },
+  {
+    hash: ["HTML", "CSS", "Responsive"],
+    title: "Recipe Blog",
+    description:
+      "In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io",
+    demo: "https://bashidagha.github.io/recipe-page-master/",
+    code: "https://github.com/bashidagha/recipe-page-master",
+    photo: "p1.webp",
+  },
+  {
+    hash: ["HTML", "CSS", "Responsive"],
+    title: "Recipe Blog",
+    description:
+      "In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io",
+    demo: "https://bashidagha.github.io/recipe-page-master/",
+    code: "https://github.com/bashidagha/recipe-page-master",
+    photo: "p1.webp",
+  },
+  {
+    hash: ["HTML", "CSS", "Responsive"],
+    title: "Recipe Blog",
+    description:
+      "In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io",
+    demo: "https://bashidagha.github.io/recipe-page-master/",
+    code: "https://github.com/bashidagha/recipe-page-master",
+    photo: "p1.webp",
+  },
+  {
+    hash: ["HTML", "CSS", "Responsive"],
+    title: "Recipe Blog",
+    description:
+      "In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io",
+    demo: "https://bashidagha.github.io/recipe-page-master/",
+    code: "https://github.com/bashidagha/recipe-page-master",
+    photo: "p1.webp",
+  },
+];
+
 const Projects = () => {
+  const pagesArray = Array.from(Array(Math.ceil(myprojects.length / 3)).keys());
+
   return (
     <AnimationOnScroll
       animateIn="animate__fadeInRightBig"
@@ -10,11 +96,23 @@ const Projects = () => {
       animatePreScroll={false}
       className={classes.projects}
     >
-      <div className="tile"></div>
+      <div className={classes.projects__container}>
+        <div className="tile"></div>
 
-      <div className="tile"></div>
+        <div className="tile"></div>
 
-      <div className="tile"></div>
+        <div className="tile"></div>
+      </div>
+
+      <div className={classes.pagination}>
+        <div className={classes.page}>{"<"}</div>
+        {pagesArray.map((page) => (
+          <div className={classes.page} key={page}>
+            {page + 1}
+          </div>
+        ))}
+        <div className={classes.page}>{">"}</div>
+      </div>
     </AnimationOnScroll>
   );
 };
